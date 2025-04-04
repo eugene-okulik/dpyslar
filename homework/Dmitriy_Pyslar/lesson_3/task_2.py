eb1e7288e0e76ba4eca8e0bb165cee7fe82ad48b
Dmitriy Pyslar <dmitriy.nqa@gmail.com>
2025-04-04 14:54:48 +0300

    Correction files whithout whitespace

--- homework/Dmitriy_Pyslar/lesson_3/task_2.py
+++ homework/Dmitriy_Pyslar/lesson_3/task_2.py
@@ -1,3 +1,3 @@
 x = 13
 y = 9
-print(x-y/1+x*y)
+print(x - y / 1 + x * y)
--- homework/Dmitriy_Pyslar/lesson_3/task_3.py
+++ homework/Dmitriy_Pyslar/lesson_3/task_3.py
@@ -1,7 +1,7 @@
 a = 4
 b = 9
-avg_arithmetic = (a+b)/2
-avg_geomethric = (a*b)**0.5
+avg_arithmetic = (a + b) / 2
+avg_geomethric = (a * b) ** 0.5
 print(avg_arithmetic)
 print(avg_geomethric)
 
