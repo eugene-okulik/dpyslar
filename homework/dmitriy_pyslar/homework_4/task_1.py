0d19d260116f3d6b357be9f3e1b5910bbaef820c
Dmitriy Pyslar <dmitriy.nqa@gmail.com>
2025-04-06 01:40:02 +0300

    added whitespaces after

--- homework/dmitriy_pyslar/homework_4/task_1.py
+++ homework/dmitriy_pyslar/homework_4/task_1.py
@@ -1,7 +1,7 @@
 my_dict = {
     'tuple': (1, 'two', True, 15.2, None),
     'list': [None, False, 2.15, 'four', 5],
-    'dict': {'true': True, 1: 15, False: 'false', (2,1): (1,2), 'intro_list': [3,2,1]},
+    'dict': {'true': True, 1: 15, False: 'false', (2, 1): (1, 2), 'intro_list': [3, 2, 1]},
     'set': {False, True, 123, 123.45, 'multiset'}
 }
 
