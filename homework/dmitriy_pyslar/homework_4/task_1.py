c5b58de29dd4ad22c7daa11e39e1d68d055ce416
Dmitriy Pyslar <dmitriy.nqa@gmail.com>
2025-04-06 01:35:53 +0300

    correction mistakes for flake8

--- homework/dmitriy_pyslar/homework_4/task_1.py
+++ homework/dmitriy_pyslar/homework_4/task_1.py
@@ -1,8 +1,8 @@
 my_dict = {
-    'tuple' : (1, 'two', True, 15.2, None),
-    'list' : [None, False, 2.15, 'four', 5],
-    'dict' : {'true': True, 1: 15, False: 'false', (2,1): (1,2), 'intro_list': [3,2,1]},
-    'set' : {False, True, 123, 123.45, 'multiset'}
+    'tuple': (1, 'two', True, 15.2, None),
+    'list': [None, False, 2.15, 'four', 5],
+    'dict': {'true': True, 1: 15, False: 'false', (2,1): (1,2), 'intro_list': [3,2,1]},
+    'set': {False, True, 123, 123.45, 'multiset'}
 }
 
 print(my_dict['tuple'][-1])
