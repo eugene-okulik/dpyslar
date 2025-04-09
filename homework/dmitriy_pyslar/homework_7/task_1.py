495db81114b279ed9a5d104303eb3bfc244a23f7
Dmitriy Pyslar <dmitriy.nqa@gmail.com>
2025-04-09 18:47:52 +0300

    homework_7 done

---
+++ homework/dmitriy_pyslar/homework_7/task_1.py
@@ -0,0 +1,9 @@
+
+while True:
+    a = 13
+    user_number = int(input('Enter a number: '))
+    if user_number == a:
+        print('Поздравляю! Вы угадали!')
+        break
+    else:
+        print('попробуйте снова')
---
+++ homework/dmitriy_pyslar/homework_7/task_3.py
@@ -0,0 +1,12 @@
+def calc_line(line):
+    list_line = line.split()
+    last_word = list_line[-1]
+    print(int(last_word) + 10)
+
+calc_line('результат операции: 42')
+calc_line('результат операции: 54')
+calc_line('результат работы программы: 209')
+calc_line('результат: 2')
+
+
+
